b155de507b41f197649d5b0408ddd1f21aa6ca5a
Thanh Phu <thanh.phu@daliworks.net>
2017-09-19 19:36:22 +0900

    Disable mongo usage and device Id check, allow all mesages

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-var server = require('./lib/server')
-  , debug  = require('debug')('lelylan');
+require('dotenv').config();
+const server = require('./lib/server');
+const debug = require('debug')('lelylan');
 
-var ascoltatore = {
-      type: 'redis',
-      redis: require('redis'),
-      db: 12,
-      port: 6379,
-      host: process.env.REDIS_HOST }, settings = {
-      port: process.env.NODE_PORT || 1883,
-      backend: ascoltatore };
+var listener = {
+  type: 'amqp',
+  json: false,
+  client: {
+    host: 'localhost',
+    port: 5672,
+    login: 'guest',
+    password: 'guest'
+  },
+  amqp: require('amqp'),
+  exchange: 'mosca'
+};
+var settings = {
+  port: process.env.NODE_PORT || 1883,
+  backend: listener
+};
 
 var app = new server.start(settings);
 
-app.on('published', function(packet, client) {
+app.on('published', function (packet, client) {
   if (packet.topic.indexOf('$SYS') === 0) return; // doesn't print stats info
-    debug('ON PUBLISHED', packet.payload.toString(), 'on topic', packet.topic);
+  console.log('ON PUBLISHED', packet.payload.toString(), 'on topic', packet.topic);
 });
 
-app.on('ready', function() {
-  debug('MQTT Server listening on port', process.env.NODE_PORT)
+app.on('ready', function () {
+  console.log('MQTT Server listening on port', settings.port);
 });
 
